2ccaa80bd48a638381f0b82b6bd6c77173cc8c5d
Nick <cascella3509@gmail.com>
2021-11-18 13:53:39 -0500

    Using express

--- server.js
+++ server.js
@@ -1,38 +1,59 @@
-const http = require("http");
-const fs = require("fs");
+const express = require("express");
+const app = express();
 
-const server = http.createServer((req, res) => {
-  console.log(req.url);
-  res.setHeader("Content-Type", "text/html");
-  let path = "./public/";
-  switch (req.url) {
-    case "/":
-      path += "index.html";
-      res.statusCode = 200;
-      break;
-    case "/about":
-      path += "about.html";
-      res.statusCode = 200;
-      break;
-    case "/contact-me":
-      path += "contact-me.html";
-      res.statusCode = 200;
-      break;
-    default:
-      path += "404.html";
-      res.statusCode = 404;
-      break;
-  }
+app.listen(3000);
 
-  fs.readFile(path, (err, data) => {
-    if (err) {
-      console.log(err);
-      res.end();
-    }
-    res.end(data);
-  });
+app.get("/", (req, res) => {
+  res.sendFile("./public/index.html", { root: __dirname });
 });
 
-server.listen(8080, "localhost", () => {
-  console.log("listening for requests on port 8080");
+app.get("/about", (req, res) => {
+  res.sendFile("./public/about.html", { root: __dirname });
 });
+
+app.get("/contact-me", (req, res) => {
+  res.sendFile("./public/contact-me.html", { root: __dirname });
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile("./public/404.html", { root: __dirname });
+});
+
+// const http = require("http");
+// const fs = require("fs");
+
+// const server = http.createServer((req, res) => {
+//   console.log(req.url);
+//   res.setHeader("Content-Type", "text/html");
+//   let path = "./public/";
+//   switch (req.url) {
+//     case "/":
+//       path += "index.html";
+//       res.statusCode = 200;
+//       break;
+//     case "/about":
+//       path += "about.html";
+//       res.statusCode = 200;
+//       break;
+//     case "/contact-me":
+//       path += "contact-me.html";
+//       res.statusCode = 200;
+//       break;
+//     default:
+//       path += "404.html";
+//       res.statusCode = 404;
+//       break;
+//   }
+
+//   fs.readFile(path, (err, data) => {
+//     if (err) {
+//       console.log(err);
+//       res.end();
+//     }
+//     res.end(data);
+//   });
+// });
+
+// server.listen(8080, "localhost", () => {
+//   console.log("listening for requests on port 8080");
+// });
